c4851b612db02eb1b773704cd584862d00858470
ashu56_oyo <ashutosh.kumar7@oyorooms.com>
2020-06-22 00:09:13 +0530

    Removing exception message

--- src/main/java/com/employeemanagement/constant/StringConstant.java
+++ src/main/java/com/employeemanagement/constant/StringConstant.java
@@ -5,6 +5,6 @@ public class StringConstant {
     public static final String SEARCH_NAME_REGEX = "^[a-zA-Z]*[0-9]*$";
     public static final String CREATE_NAME_REGEX = "[a-zA-Z]+$";
     public static final String REQUEST_ID = "requestId";
-    public static final String CONSTRAINT_VIOLATION_MESSAGE ="Invalid input: Name must be in alphabet, age must be greater than 14 and salary must be positive";
+
 
 }
