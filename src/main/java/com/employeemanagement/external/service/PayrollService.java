bd41bf1a4106c9a8e2053af7b8969732952bfd3e
ashu56_oyo <ashutosh.kumar7@oyorooms.com>
2020-06-22 15:59:47 +0530

    Adding request headers

--- src/main/java/com/employeemanagement/external/service/PayrollService.java
+++ src/main/java/com/employeemanagement/external/service/PayrollService.java
@@ -4,6 +4,8 @@ import com.employeemanagement.dto.request.EmployeeRequest;
 import com.employeemanagement.external.RestApiManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -26,18 +28,24 @@ public class PayrollService {
 
 
     public List<PayrollEmployeeDetails> getAllEmployees(){
-        return restApiManager.get(payrollBaseUrl,payrollGetAllEndpoint,null
+        return restApiManager.get(payrollBaseUrl,payrollGetAllEndpoint, getRequestHeader()
                 , PayrollAllEmployeeResponse.class).getPayrollEmployeeDetailsList();
     }
 
     public PayrollEmployeeDetails getEmployeeById(int id){
-        return restApiManager.get(payrollBaseUrl,"/api/v1/employee/"+id,null
+        return restApiManager.get(payrollBaseUrl,"/api/v1/employee/"+id, getRequestHeader()
                 , PayrollEmployeeResponse.class).getPayrollEmployeeDetails();
     }
 
     public PayrollEmployee createEmployee(EmployeeRequest employeeRequest){
            return restApiManager.post(payrollBaseUrl, payrollCreateEndpoint, employeeRequest
-                   , null, PayrollCreateResponse.class).getPayrollEmployee();
+                   , getRequestHeader(), PayrollCreateResponse.class).getPayrollEmployee();
+    }
+
+    private HttpHeaders getRequestHeader(){
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.add(HttpHeaders.CONTENT_TYPE,MediaType.APPLICATION_JSON_VALUE);
+        return requestHeaders;
     }
 }
 
