3703fdce5de043c45a11ca3f1fc31229d1ba992e
ashu56_oyo <ashutosh.kumar7@oyorooms.com>
2020-06-22 15:58:56 +0530

    Adding valid exception message

--- src/main/java/com/employeemanagement/errorhandlers/SimpleControllerAdvice.java
+++ src/main/java/com/employeemanagement/errorhandlers/SimpleControllerAdvice.java
@@ -2,8 +2,11 @@ package com.employeemanagement.errorhandlers;
 
 import com.employeemanagement.exception.EmployeeNotFoundException;
 import com.employeemanagement.exception.PayrollServiceException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import io.swagger.annotations.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -18,8 +21,7 @@ import javax.validation.ConstraintViolationException;
 public class SimpleControllerAdvice {
      public static String EMPLOYEE_NOT_FOUND_MESSAGE = "No Response found for this request";
      public static String PAYROLL_EXCEPTION_MESSAGE = "Unable to connect to payroll service";
-     public static String INVALID_ARGUMENT_MESSAGE ="Invalid arguments";
-     public static String NUMBER_FORMAT_MESSAGE = "Type mismatch for request params";
+     public static String INVALID_FORMAT_MESSAGE = "Type mismatch for request params";
      public static  String CONSTRAINT_VIOLATION_MESSAGE ="Invalid input: Name must be in alphabet, age must be greater than 14 and salary must be positive";
 
     @ExceptionHandler(EmployeeNotFoundException.class)
@@ -29,11 +31,25 @@ public class SimpleControllerAdvice {
         return EMPLOYEE_NOT_FOUND_MESSAGE;
     }
 
+    @ExceptionHandler(InvalidFormatException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleInvalidFormatException(InvalidFormatException exception, HttpServletRequest request){
+        log.error("Invalid format for request {} ",request.getRequestURI(),exception);
+        return INVALID_FORMAT_MESSAGE;
+    }
+
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleNumberFormat(MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
+    public String handleMethodArgumentMismatch(MethodArgumentTypeMismatchException exception, HttpServletRequest request) {
         log.warn("Expecting number in place of string{} ",request.getRequestURI(),exception);
-        return NUMBER_FORMAT_MESSAGE;
+        return CONSTRAINT_VIOLATION_MESSAGE;
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMessageNotReadableException(HttpMessageNotReadableException exception, HttpServletRequest request){
+        log.warn("Invalid argument passed for request {} ",request.getRequestURI(),exception);
+        return CONSTRAINT_VIOLATION_MESSAGE;
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -41,13 +57,6 @@ public class SimpleControllerAdvice {
     public String handleMethodArgumentNotValidException(MethodArgumentNotValidException exception, HttpServletRequest request){
         log.warn("Invalid argument passed for request {} ",request.getRequestURI(),exception);
         return CONSTRAINT_VIOLATION_MESSAGE;
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleIllegalArgumentException(IllegalArgumentException exception, HttpServletRequest request){
-        log.warn("Rest api failed for request {} ",request.getRequestURI(),exception);
-        return INVALID_ARGUMENT_MESSAGE;
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
@@ -70,6 +79,5 @@ public class SimpleControllerAdvice {
         log.error("Rest api failed for request {} ",request.getRequestURI(),exception);
         return exception.getMessage();
     }
-
 
 }
